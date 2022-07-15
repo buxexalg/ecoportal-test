9803fb400dc58ee3f0ce302dbfb43ee9db80c309
buxexalg <lucasgralcantara@gmail.com>
2022-07-15 20:41:27 -0300

    feat: add getAllMoviesQuery

---
+++ coolmovies-mobile/lib/api/graphql_queries.dart
@@ -0,0 +1,22 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+final getAllMoviesQuery = gql(r"""
+          query AllMovies {
+            allMovies {
+              nodes {
+                id
+                imgUrl
+                movieDirectorId
+                userCreatorId
+                title
+                releaseDate
+                nodeId
+                userByUserCreatorId {
+                  id
+                  name
+                  nodeId
+                }
+              }
+            }
+          }
+        """);
