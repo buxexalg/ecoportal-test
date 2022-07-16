64894d8c897ab82ce843924464549c631f17878f
buxexalg <lucasgralcantara@gmail.com>
2022-07-15 22:45:07 -0300

    feat: add MovieReviews model

--- coolmovies-mobile/lib/pages/all_movies/all_movies_model.dart
+++ coolmovies-mobile/lib/pages/all_movies/all_movies_model.dart
@@ -1,26 +1,42 @@
-import 'package:coolmovies/API/graphql_client.dart';
-import 'package:coolmovies/API/graphql_queries.dart';
+import 'package:coolmovies/api/graphql_client.dart';
+import 'package:coolmovies/api/graphql_queries.dart';
 import 'package:coolmovies/model/movies_model.dart';
+import 'package:coolmovies/model/reviews_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class AllMoviesModel {
   final moviesApiClient = MoviesApiClient();
 
-  late Movies _allMovies;
+  Movies? _allMovies;
+  Movies? get allMovies => _allMovies;
 
-  Movies get allMovies => _allMovies;
+  MovieReviews? _allReviews;
+  MovieReviews? get allReviews => _allReviews;
 
   void getAllMovies(BuildContext context) async {
     final QueryResult result =
         await moviesApiClient.query(context, getAllMoviesQuery);
 
+    if (result.hasException) {
+      print(result.exception.toString());
+    }
 
     if (result.data != null) {
-      final parsedJson =
-          Movies.fromJson(result.data as Map<String, dynamic>);
+      _allMovies = Movies.fromJson(result.data!);
+    }
+  }
 
-      _allMovies = parsedJson;
+  void getAllReviews(BuildContext context) async {
+    final QueryResult result =
+        await moviesApiClient.query(context, getAllMovieReviewsQuery);
+
+    if (result.hasException) {
+      print(result.exception.toString());
+    }
+
+    if (result.data != null) {
+      _allReviews = MovieReviews.fromJson(result.data!);
     }
   }
 }
