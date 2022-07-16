079d6fae18b43eebf53a14af328705cdcd880f45
buxexalg <lucasgralcantara@gmail.com>
2022-07-16 11:36:41 -0300

    refactor: change from MVVM to QueryWrapper

--- coolmovies-mobile/lib/pages/all_movies/all_movies_view.dart
+++ coolmovies-mobile/lib/pages/all_movies/all_movies_view.dart
@@ -1,7 +1,5 @@
-import 'package:coolmovies/model/movies_model.dart';
-import 'package:coolmovies/model/reviews_model.dart';
 import 'package:coolmovies/pages/all_movies/all_movies_components.dart';
-import 'package:coolmovies/pages/all_movies/all_movies_viewmodel.dart';
+import 'package:coolmovies/pages/all_movies/all_movies_queries.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,17 +11,8 @@ class AllMoviesView extends StatefulWidget {
 }
 
 class _AllMoviesViewState extends State<AllMoviesView> {
-  final viewModel = AllMoviesViewModel();
-  final ValueNotifier<Movies?> _moviesData = ValueNotifier(null);
-  final ValueNotifier<MovieReviews?> _reviewsData = ValueNotifier(null);
-
   @override
   Widget build(BuildContext context) {
-    viewModel.getAllMovies(context);
-    viewModel.getAllReviews(context);
-    _moviesData.value = viewModel.movies;
-    _reviewsData.value = viewModel.allReviews;
-
     return Scaffold(
       backgroundColor: Color(0xff1d2226),
       body: SafeArea(
@@ -35,94 +24,12 @@ class _AllMoviesViewState extends State<AllMoviesView> {
                 TitleTextComponent('Movies'),
                 Container(
                   height: MediaQuery.of(context).size.height * 0.55,
-                  child: ListView.separated(
-                    scrollDirection: Axis.vertical,
-                    itemCount: _moviesData.value != null
-                        ? _moviesData.value!.allMovies.nodes.length
-                        : 0,
-                    separatorBuilder: (context, _) => SizedBox(
-                      width: 12,
-                    ),
-                    itemBuilder: (context, index) => ValueListenableBuilder(
-                        valueListenable: _moviesData,
-                        builder:
-                            (BuildContext context, Movies? data, Widget? _) {
-                          return data != null
-                              ? InkWell(
-                                  onTap: () {
-                                    Navigator.pushNamed(context, 'DetailScreen',
-                                        arguments:
-                                            data.allMovies.nodes[index].id);
-                                  },
-                                  child: Container(
-                                      height: 150,
-                                      margin: EdgeInsets.symmetric(
-                                          horizontal: 30, vertical: 15),
-                                      child: Stack(
-                                          alignment: Alignment.bottomLeft,
-                                          children: <Widget>[
-                                            MovieInfoComponent(
-                                                data.allMovies.nodes[index]
-                                                    .title,
-                                                convertDate(data.allMovies
-                                                    .nodes[index].releaseDate),
-                                                data
-                                                    .allMovies
-                                                    .nodes[index]
-                                                    .movieDirectorByMovieDirectorId
-                                                    .name),
-                                            PosterComponent(
-                                                data.allMovies.nodes[index]
-                                                    .imgUrl,
-                                                0.3),
-                                          ])))
-                              : Container();
-                        }),
-                  ),
+                  child: MovieCardQueryComponent(),
                 ),
                 TitleTextComponent('Top Rated Reviews'),
                 Expanded(
                   child: Container(
-                    child: ValueListenableBuilder(
-                        valueListenable: _reviewsData,
-                        builder: (BuildContext context, MovieReviews? data,
-                            Widget? _) {
-                          return data != null
-                              ? ListView.separated(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: _reviewsData.value != null
-                                      ? _reviewsData
-                                          .value!.allMovieReviews.nodes.length
-                                      : 0,
-                                  separatorBuilder: (context, _) => SizedBox(
-                                        width: 12,
-                                      ),
-                                  itemBuilder: (context, index) => InkWell(
-                                        onTap: () {
-                                          Navigator.pushNamed(
-                                              context, 'DetailScreen',
-                                              arguments: data
-                                                  .allMovieReviews
-                                                  .nodes[index]
-                                                  .movieByMovieId
-                                                  .id);
-                                        },
-                                        child: ReviewComponent(
-                                          index,
-                                          data.allMovieReviews.nodes[index]
-                                              .title,
-                                          data.allMovieReviews.nodes[index]
-                                              .rating,
-                                          data.allMovieReviews.nodes[index]
-                                              .userByUserReviewerId.name,
-                                          data.allMovieReviews.nodes[index]
-                                              .title,
-                                          data.allMovieReviews.nodes[index]
-                                              .body,
-                                        ),
-                                      ))
-                              : Container();
-                        }),
+                    child: MovieReviewCardQueryComponent(),
                   ),
                 ),
               ],
--- coolmovies-mobile/lib/pages/movie_details/movie_detail_view.dart
+++ coolmovies-mobile/lib/pages/movie_details/movie_detail_view.dart
@@ -1,6 +1,5 @@
 import 'package:coolmovies/pages/all_movies/all_movies_components.dart';
-import 'package:coolmovies/pages/all_movies/all_movies_viewmodel.dart';
-import 'package:coolmovies/pages/movie_details/movie_detail_components.dart';
+import 'package:coolmovies/pages/movie_details/movie_detail_queries.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +11,6 @@ class MovieDetailView extends StatefulWidget {
 }
 
 class _MovieDetailViewState extends State<MovieDetailView> {
-  final viewModel = AllMoviesViewModel();
-
   @override
   Widget build(BuildContext context) {
     String? id = ModalRoute.of(context)?.settings.arguments as String;
@@ -25,10 +22,8 @@ class _MovieDetailViewState extends State<MovieDetailView> {
             height: MediaQuery.of(context).size.height,
             child: Stack(
               children: <Widget>[
-                Positioned(
-                    top: 0,
-                    child: PosterDetailComponent(
-                        'https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg')),
+                Positioned(top: 0, child: PosterDetailQueryComponent(id)),
+                Container(),
                 Positioned(
                   bottom: 0,
                   child: Container(
@@ -39,21 +34,18 @@ class _MovieDetailViewState extends State<MovieDetailView> {
                       color: Color(0xff0f1c26),
                     ),
                     child: Stack(
-                      children: <Widget>[
+                      children: [
                         Container(
                           transform:
                               Matrix4.translationValues(20.0, -50.0, 0.0),
-                          child: PosterComponent(
-                              'https://images-na.ssl-images-amazon.com/images/I/81aA7hEEykL.jpg',
-                              0.40),
+                          child: MoviePosterQueryComponent(id),
                         ),
                         Column(
                           children: [
                             Container(
                               height: 20,
                             ),
-                            MovieDetailInfoComponent('Star Wars: A New Hope',
-                                '25-05-1977', 'George Lucas'),
+                            MovieDetailQueryComponent(id),
                             Container(
                               height: 5,
                             ),
@@ -63,21 +55,7 @@ class _MovieDetailViewState extends State<MovieDetailView> {
                                 Container(
                                   height:
                                       MediaQuery.of(context).size.height * 0.45,
-                                  child: ListView.separated(
-                                    scrollDirection: Axis.vertical,
-                                    itemCount: 10,
-                                    separatorBuilder: (context, _) => SizedBox(
-                                      height: 12,
-                                    ),
-                                    itemBuilder: (context, index) =>
-                                        MovieDetailReviewComponent(
-                                            index,
-                                            'Star Wars',
-                                            '5',
-                                            'Chrono',
-                                            'Great Movie',
-                                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-                                  ),
+                                  child: MovieDetailReviewQueryComponent(id),
                                 ),
                               ],
                             )
